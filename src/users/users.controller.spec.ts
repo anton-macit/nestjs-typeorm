25c65892d72feb9a6b8b49076e9c24ab9cf860be
Anton Petrov <anton@zardi.ru>
2024-05-26 12:47:10 +0400

    Fix tests and add jest test to precommit

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,17 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { repositoryMockFactory } from '../utils/test';
+import { User } from './user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  // todo let repositoryMock: MockType<Repository<User>>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService, // Provide your mock instead of the actual repository
+        {
+          provide: getRepositoryToken(User),
+          useFactory: repositoryMockFactory,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    // todo repositoryMock = module.get(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,15 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { repositoryMockFactory } from '../utils/test';
 
 describe('UsersService', () => {
   let service: UsersService;
+  // todo let repositoryMock: MockType<Repository<User>>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        // Provide your mock instead of the actual repository
+        {
+          provide: getRepositoryToken(User),
+          useFactory: repositoryMockFactory,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    // todo repositoryMock = module.get(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
